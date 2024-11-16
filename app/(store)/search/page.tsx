7055cc79b7cf80801a26c8244c745f58868a8e6c
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-16 20:14:40 +0530

    passkey clerk made

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+async function Search({
+    searchParams,
+}: {
+    searchParams: {
+        query: string;
+    };
+}) {
+    const { query } = await searchParams;
+
+  return (
+    <div>Search for {query}</div>
+  )
+}
+
+export default Search
