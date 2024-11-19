f50645e62da0c058658b1a1bad073d4a419f5314
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-19 14:04:19 +0530

    schemas done

--- sanity/schemaTypes/orderType.ts
+++ sanity/schemaTypes/orderType.ts
@@ -120,10 +120,27 @@ export const orderType = defineType({
       },
     }),
     defineField({
-        name: "orderDate",
-        title: "Order Date",
-        type: "datetime",
-        validation: (Rule) => Rule.required(),
-    })
+      name: "orderDate",
+      title: "Order Date",
+      type: "datetime",
+      validation: (Rule) => Rule.required(),
+    }),
   ],
+  preview: {
+    select: {
+      name: "customName",
+      amount: "totalPrice",
+      currency: "currency",
+      orderId: "orderNumber",
+      email: "email",
+    },
+    prepare(select) {
+      const orderIdSnippet = `${select.orderId.slice(0, 5)}...${select.orderId.slice(-5)}`;
+      return {
+        title: `${select.name} (${orderIdSnippet})`,
+        subtitle: `${select.amount} ${select.currency}, ${select.email}`,
+        media: BasketIcon,
+      };
+    },
+  },
 });
