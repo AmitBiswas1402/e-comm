9d607184ba675b75695f3317159e722f91bd0b80
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-21 20:42:01 +0530

    groq query and home screen grid made

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,8 +1,18 @@
+import { ProductsView } from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts()
+  const categories = await getAllCategories()
+
   return (
-    <>
-      Hello
-    </>
+    <div>
+      <h1>Hello World</h1>
+      
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={categories} />
+      </div>
+    </div>
   );
 }
---
+++ components/ProductsView.tsx
@@ -0,0 +1,24 @@
+import { Category, Product } from "@/sanity.types";
+
+interface ProductViewProps {
+  products: Product[];
+  categories: Category[];
+}
+
+export const ProductsView = ({ products }: ProductViewProps) => {
+  return <div className="flex flex-col">
+    {/* categories */}
+    <div className="w-full sm:w-[200px]">
+        {/* <CategorySelectorComponent categories={categories} /> */}
+    </div>
+
+    {/* products */}
+    <div className="flex-1">
+        <div>
+            {/* <ProductGrid products={products} /> */}
+
+            <hr className="w-1/2 sm:s-3/4" />
+        </div>
+    </div>
+  </div>;
+};
